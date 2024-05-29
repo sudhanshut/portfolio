8347e4a9d3e8ff52109f9843074c271db14127a7
sudhanshut <sudhanshu.01@hotmail.com>
2024-05-29 09:53:39 +0530

    created hero file and config tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        colors:{
+          black:{
+            DEFAULT: '#000',
+            100: "#000319",
+            
+          }
+        }
     },
   },
   plugins: [],
